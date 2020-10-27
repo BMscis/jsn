e30c66ff05ece937a44e252de0e013ff69373146
BMscis <melvinwafula@gmail.com>
2020-10-27 22:52:44 +0300

    closed attributes

--- src/index.js
+++ src/index.js
@@ -4,28 +4,34 @@ import {content} from'./content/content'
 import {profile} from'./profile/profile'
 import {footer} from './components/footer/footer'
 import './index.css'
+import ood from './img/outofordersign.png'
 
 document.body.classList.add('body');
-document.body.appendChild(navbar());
-function torsoe(){
-    const tor = document.createElement('div');
-    tor.classList.add('torsoe')
-    tor.setAttribute('id', 'home')
-    tor.appendChild(home());
-    tor.appendChild(content());
-    tor.appendChild(profile());
-    return tor
-}
+document.body.classList.add('closed');
+const outoforder = document.createElement('img')
+outoforder.setAttribute('src', ood)
+outoforder.style.margin = 'auto'
+document.body.appendChild(outoforder)
+// document.body.appendChild(navbar());
+// function torsoe(){
+//     const tor = document.createElement('div');
+//     tor.classList.add('torsoe')
+//     tor.setAttribute('id', 'home')
+//     tor.appendChild(home());
+//     tor.appendChild(content());
+//     tor.appendChild(profile());
+//     return tor
+// }
 
-let tors = torsoe();
-document.body.appendChild(tors);
-document.body.appendChild(footer());
+// let tors = torsoe();
+// document.body.appendChild(tors);
+// document.body.appendChild(footer());
 
 
-window.addEventListener('scroll', e=>{
-    let menu_ls = document.getElementsByClassName('menulist')[0]
-    if(menu_ls.classList.contains('active')){
-        menu_ls.classList.remove('active')
-    }
-})
+// window.addEventListener('scroll', e=>{
+//     let menu_ls = document.getElementsByClassName('menulist')[0]
+//     if(menu_ls.classList.contains('active')){
+//         menu_ls.classList.remove('active')
+//     }
+// })
 
